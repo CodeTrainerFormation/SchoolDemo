cc69c773de45433ada283916928a883f9df927d2
Nab <17692533+Nab69@users.noreply.github.com>
2025-01-21 14:59:15 +0100

    services

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,18 @@
 using Dal;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<SchoolContext>();
+
+builder.Services.AddDbContext<SchoolContext>(
+    optionsBuilder => optionsBuilder.UseSqlServer(
+        builder.Configuration.GetConnectionString("SchoolDb")
+    )
+);
 
 var app = builder.Build();
 
