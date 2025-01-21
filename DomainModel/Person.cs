0bc546682ef9648cbe02ca855fcf754fd220b87c
Nab <17692533+Nab69@users.noreply.github.com>
2025-01-21 12:20:15 +0100

    attributes

--- DomainModel/Person.cs
+++ DomainModel/Person.cs
@@ -1,11 +1,24 @@
-﻿namespace DomainModel
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DomainModel
 {
+    [Table("Person")]
     public record Person
     {
+        [Key]
         public int PersonID { get; init; }
+
+        [StringLength(50)]
         public required string FirstName { get; set; }
+
+        [StringLength(100)]
         public required string LastName { get; set; }
+
+        [StringLength(50)]
         public string? Title { get; set; }
+
+        [Range(0, 140)]
         public int? Age { get; set; }
 
         public School? School { get; set; }
--- DomainModel/School.cs
+++ DomainModel/School.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DomainModel
 {
+    [Table("School")]
     public record School
     {
+        [Key]
         public int SchoolID { get; init; }
+
+        [StringLength(50)]
+        [Required]
         public required string Name { get; set; }
 
         public ICollection<Person> People { get; set; } = new HashSet<Person>();
