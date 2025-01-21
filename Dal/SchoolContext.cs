8ca792fdb92a3fbb4e3b49dae67a56065df33aa4
Nab <17692533+Nab69@users.noreply.github.com>
2025-01-21 12:16:05 +0100

    context

---
+++ Dal/SchoolContext.cs
@@ -0,0 +1,12 @@
+﻿using DomainModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dal
+{
+    public class SchoolContext
+    {
+        public DbSet<Person> People { get; set; } = null!;
+        public DbSet<School> Schools { get; set; } = null!;
+
+    }
+}
