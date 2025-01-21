5399717f5dc2bb3d631c87879e0bb8ad82cdd354
Nab <17692533+Nab69@users.noreply.github.com>
2025-01-21 11:48:55 +0100

    Add project files.

---
+++ DomainModel/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace DomainModel
+{
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Title { get; set; }
+        public int? Age { get; set; }
+    }
+}
---
+++ DomainModel/School.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    internal class School
+    {
+        public int SchoolID { get; set; }
+        public required string Name { get; set; }
+
+    }
+}
