3305d65425391f0445530396722477b3670feac7
Nab <17692533+Nab69@users.noreply.github.com>
2025-01-23 10:28:55 +0100

    school crud

--- WebApi/Controllers/SchoolController.cs
+++ WebApi/Controllers/SchoolController.cs
@@ -24,14 +24,62 @@ namespace WebApi.Controllers
             return Ok(list);
         }
 
+        // GET : api/school/3
+        [HttpGet("{id}")]
+        public IActionResult GetSchool([FromRoute] int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            School? school = this.context.Schools.Find(id);
+
+            if (school == null)
+                return NotFound();
+
+            return Ok(school);
+        }
+
         // POST : api/school
         [HttpPost]
-        public IActionResult AddSchool(School school)
+        public IActionResult AddSchool([FromBody] School school)
         {
             this.context.Schools.Add(school);
             this.context.SaveChanges();
 
             return Created($"school/{school.SchoolID}", school);
+        }
+
+        // PUT : api/school/3
+        [HttpPut("{id}")]
+        public IActionResult EditSchool([FromRoute] int id, [FromBody] School school)
+        {
+            if(id != school.SchoolID)
+                return BadRequest();
+
+            if(!this.context.Schools.Any(s => s.SchoolID == id))
+                return NotFound();
+
+            this.context.Schools.Update(school);
+            this.context.SaveChanges();
+
+            return NoContent();
+        }
+
+        // DELETE : api/school/3
+        [HttpDelete("{id}")]
+        public IActionResult RemoveSchool([FromRoute] int id)
+        {
+            if(id <= 0)
+                return BadRequest();
+
+            School? school = this.context.Schools.Find(id);
+            if(school == null)
+                return NotFound();
+
+            this.context.Schools.Remove(school);
+            this.context.SaveChanges();
+
+            return Ok(school);
         }
     }
 }
