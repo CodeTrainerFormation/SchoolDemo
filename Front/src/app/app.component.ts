81fc77d393f265e10dbc211befbf045b09d7b3c0
Romain Rukse <17692533+Nab69@users.noreply.github.com>
2025-01-30 13:19:18 +0100

    list schools

--- Front/src/app/app.component.ts
+++ Front/src/app/app.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SchoolService } from './school.service';
+import { School } from './school';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Bonjour tout le monde';
+export class AppComponent implements OnInit {
+  
+  // title = 'Bonjour tout le monde';
+  // items = [
+  //   "item1",
+  //   "item2",
+  //   "item3"
+  // ]
 
-  items = [
-    "item1",
-    "item2",
-    "item3"
-  ]
+
+  schools: School[] = [];
+
+  constructor(
+    private service: SchoolService
+  ){}
+
+  ngOnInit(): void {
+    this.service.getSchools()
+                .subscribe(schools => this.schools = schools);
+  }
 
   addItem(){
-    this.items.push("new item");
+    // this.items.push("new item");
   }
 }
